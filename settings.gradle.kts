4452747ec422ecf0eef6bafa83470f01798860d3
Yomamaeatstoes <114538961+Yomamaeatstoes@users.noreply.github.com>
2026-03-21 02:32:23 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,11 @@ include("Bukkit", "API")
 
 pluginManagement {
     repositories {
+        mavenLocal()
         gradlePluginPortal()
+        mavenCentral()
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://jitpack.io")
         maven("https://repo.preva1l.info/releases/")
     }
 }
